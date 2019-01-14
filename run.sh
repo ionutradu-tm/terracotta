7dbdbe6c7f104d3d0b17999945fd64fe6e7aa7c9
Ionut Radu <iradu@iradu.ro>
2019-01-14 14:44:10 +0200

    Build for DC/OS

--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@
 # before starting the terracotta server, we update the tc-config.xml configuration file
 sed -i -r 's/OFFHEAP_ENABLED/'$OFFHEAP_ENABLED'/; s/OFFHEAP_MAX_SIZE/'$OFFHEAP_MAX_SIZE'/; s/TC_SERVER1/'$TC_SERVER1'/g; s/TC_SERVER2/'$TC_SERVER2'/g' config/tc-config*.xml 
 
-if [ \( -n $TC_SERVER1 \) -o  \( -n $TC_SERVER2 \) ]; then 
+if [[ -n $TC_SERVER1 ]] || [[ -n $TC_SERVER2 ]]; then 
   echo "$HOST  $TC_SERVER1" >> /etc/hosts; 
   export HOSTNAME=$TC_SERVER1; 
   sleep 120; 
   bin/start-tc-server.sh -f config/tc-config-active-passive.xml -n $HOSTNAME; 
 else 
+  echo "127.0.0.1 terracotta.marathon.mesos" >> /etc/hosts
   bin/start-tc-server.sh -f config/tc-config-single-node.xml; 
 fi
