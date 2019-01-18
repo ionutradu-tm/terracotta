ba40b3efc152afdc90a7860da3e50cf71fddc9ef
iradu <ionut.radu@tremend.com>
2019-01-18 11:12:04 +0200

    - Update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ fi
 sed -i -r 's/OFFHEAP_ENABLED/'$OFFHEAP_ENABLED'/; s/OFFHEAP_MAX_SIZE/'$OFFHEAP_MAX_SIZE'/; s/TC_SERVER1/'$TC_SERVER1'/g; s/TC_SERVER2/'$TC_SERVER2'/; s/MY_HOST/'$HOSTNAME'/g; s/TC_SERVER2/'$TC_SERVER2'/g' config/tc-config*.xml 
 
 if [[ -n $TC_SERVER1 ]] || [[ -n $TC_SERVER2 ]]; then 
-  echo "$HOST  $TC_SERVER1" >> /etc/hosts; 
+  echo "127.0.0.1  $TC_SERVER1" >> /etc/hosts;
   export HOSTNAME=$TC_SERVER1; 
   sleep 120; 
   bin/start-tc-server.sh -f config/tc-config-active-passive.xml -n $HOSTNAME; 
